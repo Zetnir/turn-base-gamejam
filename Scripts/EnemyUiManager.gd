5423b09c0e30bbd733f7fea09ddc81c56631f3d6
Ludovic Bonheur <lbonheur@angelsoftwares.ca>
2026-05-09 19:54:16 -0400

    debuffIcons, light attack rm

--- Scripts/EnemyUiManager.gd
+++ Scripts/EnemyUiManager.gd
@@ -2,9 +2,11 @@ extends UiManager
 
 @export var attack_icon: TextureRect
 @export var defense_icon: TextureRect
-@export var debuff_icon: TextureRect
+@export var atk_debuff_icon: TextureRect
+@export var def_debuff_icon: TextureRect
 
-enum ActionType { ATTACK, DEFENSE, DEBUFF }
+
+enum ActionType { ATTACK, DEFENSE, ATK_DEBUFF, DEF_DEBUFF }
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,7 +16,8 @@ func show_action_type_prediction(level: ActionType) -> void:
 	match level: 
 		ActionType.ATTACK: attack_icon.visible = true
 		ActionType.DEFENSE: defense_icon.visible = true
-		ActionType.DEBUFF: debuff_icon.visible = true
+		ActionType.ATK_DEBUFF: atk_debuff_icon.visible = true
+		ActionType.DEF_DEBUFF: def_debuff_icon.visible = true
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,4 +27,5 @@ func hide_action_prediction() -> void:
 func _hide_all_action_icons() -> void:
 	attack_icon.visible = false
 	defense_icon.visible = false
-	debuff_icon.visible = false
+	atk_debuff_icon.visible = false
+	def_debuff_icon.visible = false
