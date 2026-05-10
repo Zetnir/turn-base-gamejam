ab6fb69912ab977b7247b814a7e64e6200d4a7cf
Ludovic Bonheur <lbonheur@angelsoftwares.ca>
2026-05-09 23:15:51 -0400

    ui manager, icons

--- Scripts/UI/UiManager.gd
+++ Scripts/UI/UiManager.gd
@@ -2,10 +2,24 @@ extends Control
 class_name UiManager
 
 @export var health_bar: TextureProgressBar
+@export var health_point_current: Label
+@export var shield_icon: TextureRect
+@export var shield_point_current: Label
 
 # HEALTH BAR
 func update_current_hp(current:int)-> void:
+	health_point_current.text = str(current)
 	health_bar.value = current
-
+	
 func update_max_hp(current:int)-> void:
 	health_bar.max_value = current
+	
+# SHIELD BLOCK POINTS
+func update_shield_point(current:int)-> void:
+	shield_point_current.text = str(current)
+	
+func show_shield_icon() -> void: 
+	shield_icon.visible = true
+	
+func _hide_shield_icon() -> void:
+	shield_icon.visible = false
