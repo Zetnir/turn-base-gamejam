f4c8922645d604538a9b5d850f1537c4091254e0
Ludovic Bonheur <lbonheur@angelsoftwares.ca>
2026-05-10 10:36:14 -0400

    vfx manager, anim change

---
+++ Scripts/UI/vfx_manager.gd
@@ -0,0 +1,54 @@
+extends Node2D
+@export var vfx_sprite_2d_01: AnimatedSprite2D
+@export var vfx_sprite_2d_02: AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_hide_all()
+
+
+# VFX PLAY
+func play_hit(target_position: Vector2) -> void:
+	global_position = target_position
+	vfx_sprite_2d_01.visible = true
+	vfx_sprite_2d_01.play("hit")
+	await vfx_sprite_2d_01.animation_finished
+	vfx_sprite_2d_01.visible = false
+	
+	
+func play_atkDebuff(target_position: Vector2) -> void:
+	global_position = target_position
+	vfx_sprite_2d_01.visible = true
+	vfx_sprite_2d_01.play("atkDebuff")
+	await vfx_sprite_2d_01.animation_finished
+	vfx_sprite_2d_01.visible = false
+	
+	
+func play_defDebuff(target_position: Vector2) -> void:
+	global_position = target_position
+	vfx_sprite_2d_01.visible = true
+	vfx_sprite_2d_01.play("defDebuff")
+	await vfx_sprite_2d_01.animation_finished
+	vfx_sprite_2d_01.visible = false
+	
+	
+func play_guard(target_position: Vector2) -> void:
+	global_position = target_position
+	vfx_sprite_2d_01.visible = true
+	vfx_sprite_2d_01.play("guard")
+	await vfx_sprite_2d_01.animation_finished
+	vfx_sprite_2d_01.visible = false
+	
+	
+func play_provocation(target_position: Vector2) -> void:
+	global_position = target_position
+	vfx_sprite_2d_02.visible = true
+	vfx_sprite_2d_02.play("provocation")
+	await vfx_sprite_2d_02.animation_finished
+	vfx_sprite_2d_02.visible = false
+	
+# HIDE ALL VFX SPRITES
+func _hide_all() -> void:
+	vfx_sprite_2d_01.visible = false
+	vfx_sprite_2d_02.visible = false
